f1f3f0ff31ac426b485438597c8c107994b015c1
PhilipeX <dev.philipe@outlook.com>
2025-01-08 23:11:58 -0300

    wip(country card): creates service and interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './components/header/header.component';
+import { CountryCardComponent } from './components/country-card/country-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CountryCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/country-summary.interface.ts
@@ -0,0 +1,7 @@
+export interface CountrySummaryInterface {
+  flag: string;
+  name: string;
+  population: number;
+  region: string;
+  capital: string;
+}
