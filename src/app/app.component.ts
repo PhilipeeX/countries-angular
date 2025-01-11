25d06d4cf42cc1caeddf8017732496485a4015af
PhilipeX <dev.philipe@outlook.com>
2025-01-10 23:17:47 -0300

    feat(filter): creates search by country name filter functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,33 @@
-import {Component, OnInit} from '@angular/core';
-import {CountrySummaryInterface} from './models/country-summary.interface';
-import {CountryService} from './services/country.service';
+import { Component, OnInit } from '@angular/core';
+import { CountrySummaryInterface } from './models/country-summary.interface';
+import { CountryService } from './services/country.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
 export class AppComponent implements OnInit {
   title = 'countries-angular';
   countries: CountrySummaryInterface[] = [];
+  filteredCountries: CountrySummaryInterface[] = [];
 
   constructor(private countryService: CountryService) {}
 
   ngOnInit(): void {
     this.countryService.getCountries().subscribe((data: CountrySummaryInterface[]) => {
       this.countries = data;
+      this.filteredCountries = data;
     });
+  }
+
+  onFilterChanged(searchTerm: string): void {
+    if (!searchTerm) {
+      this.filteredCountries = this.countries;
+    } else {
+      this.filteredCountries = this.countries.filter(country =>
+        country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
   }
 }
