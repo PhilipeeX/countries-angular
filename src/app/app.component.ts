4ca87a7924eff53fb29b5115db3a88c179a380d9
Philipe Rodrigues <85847179+PhilipeeX@users.noreply.github.com>
2025-04-01 00:28:10 -0300

    Revert "Feat/7 permitir que usuarios acessem pagina de detalhes de cada pais"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CountrySummaryInterface } from './models/country-summary.interface';
+import { CountryService } from './services/country.service';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'countries-angular';
+  countries: CountrySummaryInterface[] = [];
+  filteredCountries: CountrySummaryInterface[] = [];
+  searchTerm = '';
+  selectedRegion = '';
 
-  constructor() {}
+  constructor(private countryService: CountryService) {}
 
   ngOnInit(): void {
+    this.countryService.getCountries().subscribe((data: CountrySummaryInterface[]) => {
+      this.countries = data;
+      this.filteredCountries = data;
+    });
+  }
+
+  onFilterChanged(filters: { searchTerm: string, selectedRegion: string }): void {
+    this.searchTerm = filters.searchTerm;
+    this.selectedRegion = filters.selectedRegion;
+    this.filterCountries();
+  }
+
+  filterCountries(): void {
+    this.filteredCountries = this.countries.filter(country => {
+      const matchesSearchTerm = country.name.common.toLowerCase().includes(this.searchTerm.toLowerCase());
+      const matchesRegion = this.selectedRegion ? country.region === this.selectedRegion : true;
+      return matchesSearchTerm && matchesRegion;
+    });
   }
 }
--- src/app/services/country.service.ts
+++ src/app/services/country.service.ts
@@ -2,26 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CountrySummaryInterface } from '../models/country-summary.interface';
-import {CountryDetailsInterface} from '../models/country-details.interface';
-import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
-  private apiUrl = 'https://restcountries.com/v3.1';
-  private filters = '/all?fields=name,population,region,capital,flags';
+
+  private apiUrl = 'https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags';
 
   constructor(private http: HttpClient) { }
 
   getCountries(): Observable<CountrySummaryInterface[]> {
-    return this.http.get<CountrySummaryInterface[]>(`${this.apiUrl}${this.filters}`);
-  }
-
-  getCountryByName(name: string): Observable<CountryDetailsInterface> {
-    return this.http.get<CountryDetailsInterface[]>(`${this.apiUrl}/name/${name}?fullText=true`)
-      .pipe(
-        map((response: CountryDetailsInterface[]) => response[0])
-      );
+    return this.http.get<CountrySummaryInterface[]>(this.apiUrl);
   }
 }
